8a43f9c861cc8f0ecfb39c03f09a0c88db86c2ae
AbdelI <homeworkoracle@protonmail.com>
2020-01-16 13:45:18 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const port = process.env.PORT || 4400;
+
+const app = express();
+
+app.use(express.static("client/build"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/welcome", (req, res) => {
+  res.json(`hello, ${req.query.name}! welcome to my website`);
+});
+
+app.use((req, res) => {
+  res.json("The Api works");
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
